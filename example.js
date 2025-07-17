07fe013034042ee4e13cee0891620db81f5ed760
Aarmaaa28 <aarmaa28@gmail.com>
2025-07-18 06:32:26 +0800

    Update example.js

--- example.js
+++ example.js
@@ -1,6 +1,6 @@
-import { Client, GatewayIntentBits, Partials, Events } from 'discord.js'
-import { uploadTop4Top } from './top4top.js'
+import { Client, GatewayIntentBits, Partials } from 'discord.js'
 import axios from 'axios'
+import uploadTop4Top from './top4top.js'
 
 const client = new Client({
   intents: [
@@ -15,22 +15,18 @@ const client = new Client({
   ]
 })
 
-client.once(Events.ClientReady, () => {
+client.once('ready', () => {
   console.log(`Bot ready as ${client.user.tag}`)
 })
 
-client.on(Events.MessageCreate, async (message) => {
-  if (message.author.bot || !message.content.startsWith('!up')) return
-
-  if (!message.attachments.size) {
-    return message.reply('Please attach the file along with the command `!up`.')
-  }
-
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return
+  if (!message.content.startsWith('!up')) return
+  if (!message.attachments.size) return message.reply('Please attach the file along with the command `!up`.')
   try {
     const attachment = message.attachments.first()
-    const response = await axios.get(attachment.url, { responseType: 'arraybuffer' })
-
-    const url = await uploadTop4Top(Buffer.from(response.data), attachment.name)
+    const data = await axios.get(attachment.url, { responseType: 'arraybuffer' })
+    const url = await uploadTop4Top(Buffer.from(data.data), attachment.name)
     await message.reply(`Successfully uploaded to Top4Top:\n${url}`)
   } catch (error) {
     console.error('Uploads unsuccessful:', error)
